0d6d839ddd1377008fe7f78c3f1eb2f8a63cf11d
XVincentTu <tchihchun@gmail.com>
2019-02-23 16:23:10 +0800

    Update homework on 2019-02-23 3rd ( Optimize Java Code )

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,11 +4,41 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.print("Please enter number of tickets: ");
+        int total_num = -1, round_num = -1;
         Scanner scanner = new Scanner(System.in);
-        int total_num = scanner.nextInt();
-        System.out.print("How many round-trip tickets: ");
-        int round_num = scanner.nextInt();
+        do {
+            // Check Total Number of Tickets ( Single plus Round-Trip )
+            System.out.print("Please enter number of tickets: ");
+            String input = scanner.next();
+            try {
+                total_num = Integer.parseInt(input);
+                // Number of Tickets < 1 is illegal
+                if (total_num < 1) {
+                    System.out.println("Invalid Ticket Number! (Negative Number Wrong)");
+                }
+            } catch (NumberFormatException e) {
+                // Number of Tickets must be Integer
+                System.out.println("Invalid Ticket Number! (Non Integer Wrong)");
+            }
+        } while (total_num < 1);
+        do {
+            // Check Total Round-Trip Number of Tickets
+            System.out.print("How many round-trip tickets: ");
+            String input = scanner.next();
+            try {
+                round_num= Integer.parseInt(input);
+                // Number of Tickets < 1 is illegal
+                if (round_num < 1) {
+                    System.out.println("Invalid Round-Trip Tickets! (Negative Number Wrong)");
+                // Number of Round-Trip Tickets must be lower than Number of Total Tickets
+                } else if (round_num > total_num) {
+                    System.out.println("Invalid Round-Trip Tickets! (Round-Trip Tickets> Total Tickets)");
+                }
+            } catch (NumberFormatException e) {
+                // Number of Tickets must be Integer
+                System.out.println("Invalid Round-Trip Tickets! (Non Integer Wrong)");
+            }
+        } while (round_num < 1 || round_num > total_num);
         Ticket ticket = new Ticket(total_num, round_num);
         ticket.print();
     }
